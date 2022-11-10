cb125be555bf3629d7cb4db87ed9ab86339120b2
Eduardo Barros <eduardo.barros@executiva.com.br>
2022-11-09 23:57:35 -0300

    Update Code

--- app/Console/Commands/Invoices.php
+++ app/Console/Commands/Invoices.php
@@ -2,9 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Invoice;
 use App\Services\SignetInvoiceService;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Console\Command;
+use Nette\Utils\Json;
+use Nette\Utils\JsonException;
 
 class Invoices extends Command
 {
@@ -46,6 +49,7 @@ class Invoices extends Command
      *
      * @return void
      * @throws GuzzleException
+     * @throws JsonException
      */
     public function handle()
     {
@@ -56,6 +60,33 @@ class Invoices extends Command
 
         $data = $this->signetInvoiceService->getListInvoices($params);
 
-        dd($data);
+        if ($data) {
+
+            $output = Json::decode($data, 1);
+
+            foreach ($output['detail'] as $row) {
+                Invoice::updateOrCreate(
+                    [
+                        'invoice' => $row['invoice'],
+                    ],
+                    [
+                        'customer_code' => $row['customer_code'],
+                        'company_name' => $row['company_name'],
+                        'cnpj' => $row['cnpj'],
+                        'status' => $row['status'],
+                        'qty' => $row['qty'],
+                        'amount' => $row['amount'],
+                        'total' => $row['total'],
+                        'paid' => $row['paid'],
+                        'paid_date' => $row['paid_date'],
+                        'invoice_date' => $row['invoice_date'],
+                        'invoice_duedate' => $row['invoice_duedate'],
+                        'invoice_number' => $row['invoice_number'],
+                        'invoice_key' => $row['invoice_key'],
+                        'invoice_string' => $row['invoice_string'],
+                        'link_nfe' => $row['linkNFe'],
+                    ]);
+            }
+        }
     }
 }
