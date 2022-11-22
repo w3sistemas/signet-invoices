fd34fccde8287552c964b5fcbe61beafa3aa4649
Eduardo Barros <eduardo.barros@executiva.com.br>
2022-11-22 02:32:18 -0300

    Update Code

--- app/Enumerators/SignetEnum.php
+++ app/Enumerators/SignetEnum.php
@@ -15,4 +15,11 @@ class SignetEnum
         'ABN',
         'ABN7'
     ];
+
+    CONST BANKS = [
+        '237' => 'BANCO BRADESCO S.A',
+        '341' => 'ITAÚ UNIBANCO S.A',
+        '339' => 'BANCO SANTANDER (BRASIL) S.A',
+        '707' => 'BANCO DAYCOVAL S.A.'
+    ];
 }
