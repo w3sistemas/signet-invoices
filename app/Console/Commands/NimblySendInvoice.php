5045e09f42a8925ce0b0764b5beafa3eb4cf0359
Eduardo Barros <eduardo.barros@executiva.com.br>
2022-11-22 18:49:59 -0300

    Update Code

--- app/Console/Commands/NimblySendInvoice.php
+++ app/Console/Commands/NimblySendInvoice.php
@@ -63,6 +63,7 @@ class NimblySendInvoice extends Command
 
         if ($invoices) {
             foreach ($invoices as $invoice){
+
                 $clientRemote = Client::where('code', $invoice['customer_code'])->first();
 
                 /*
@@ -108,73 +109,66 @@ class NimblySendInvoice extends Command
                             }
                         }
                     }
-                }
 
-                /*
+                    /*
                  * Grava Rececimento
                  * */
-                $params = [
-                    'ID' => 0,
-                    'Descri' => 'string',
-                    'DtaVenc' => $invoice['invoice_duedate'],
-                    'VlrVenc' => $invoice['total'],
-                    'VlrBruto' => $invoice['amount'],
-                    'IDPessoa' => $idClient,
-                    'IDCentroCusto' => null,
-                    'DtaPagto' => $invoice['paid_date'],
-                    'VlrPagto' => $invoice['paid'],
-                    'DtaCompet' => $invoice['invoice_date'],
-                    'IDTipoReceb' => 10,
-                    'DataLimiteDesconto' => $invoice['invoice_duedate'],
-					'DataVencAtual' => $invoice['invoice_duedate'],
-					'NroDoc' => $invoice['invoice_number'],
-					'LinkNFSe' => $invoice['link_nfe']
-                ];
+                    $params = [
+                        'ID' => 0,
+                        'Descri' => 'string',
+                        'DtaVenc' => $invoice['invoice_duedate'],
+                        'VlrVenc' => $invoice['total'],
+                        'VlrBruto' => $invoice['amount'],
+                        'IDPessoa' => $idClient,
+                        'IDCentroCusto' => null,
+                        'DtaPagto' => $invoice['paid_date'],
+                        'VlrPagto' => $invoice['paid'],
+                        'DtaCompet' => $invoice['invoice_date'],
+                        'IDTipoReceb' => 10,
+                        'DataLimiteDesconto' => $invoice['invoice_duedate'],
+                        'DataVencAtual' => $invoice['invoice_duedate'],
+                        'NroDoc' => $invoice['invoice_number'],
+                        'LinkNFSe' => $invoice['link_nfe']
+                    ];
 
-                $request = $this->nimblyInvoiceService->createOrUpdateInvoice($params);
+                    $request = $this->nimblyInvoiceService->createOrUpdateInvoice($params);
 
-                $dataInvoice = Json::decode($request, 1);
+                    $dataInvoice = Json::decode($request, 1);
 
-                /*
-                 * Grava Dados Boleto
-                 * */
-                $ticketData = [
-                    'ID' => 0,
-                    'DataHoraEmissao' => $invoice['invoice_date'],
-                    'DataVencimento' => $invoice['invoice_duedate'],
-                    'Valor' => $invoice['total'],
-                    'LinhaDigitavel' => $invoice['invoice_string'],
-                    'Descricao' => 'Boleto',
-                    'IDContaRec' => 0,
-                    'CodigoBancoDigito' => 0,
-                    'Instrucoes' => 'Pagar',
-                    'IDContaBanco' => 0,
-                    'IDTipoRecebimento' => 0,
-                    'NossoNumero' => 0,
-                    'NossoNumeroFormatado' => 0,
-                    'IDPessoaCedente' => $idClient,
-                    'IDPessoaSacado' => 0,
-                    'AgenciaCodigoCedente' => 0,
-                    'CarteiraCobranca' => 0,
-                    'NumeroDocumento' => $clientRemote->document,
-                    'CodigoBarras' => 0,
-                    'ValorJurosDiario' => 0,
-                    'ValorMulta' => 0,
-                    'ValorDesconto' => 0
-                ];
+                    /*
+                     * Grava Dados Boleto
+                     * */
+                    $rates = getRatesByBank($invoice['total']);
+                    $ticketData = [
+                        'ID' => 0,
+                        'DataHoraEmissao' => $invoice['invoice_date'],
+                        'DataVencimento' => $invoice['invoice_duedate'],
+                        'Valor' => $invoice['total'],
+                        'LinhaDigitavel' => $invoice['invoice_string'],
+                        'IDContaRec' => $dataInvoice['ID'],
+                        'NossoNumero' => $invoice['our_number'],
+                        'NossoNumeroFormatado' => $invoice['our_number'],
+                        'IDPessoaCedente' => 0,
+                        'IDPessoaSacado' => $idClient,
+                        'NumeroDocumento' => $clientRemote->document,
+                        'ValorJurosDiario' => $rates['day'] ?? 0,
+                        'ValorMulta' => $rates['fine'] ?? 0,
+                        'ValorDesconto' => $invoice['discounts']
+                    ];
 
-                $request = $this->nimblyInvoiceService->sentTicketData($ticketData);
+                    $request = $this->nimblyInvoiceService->sentTicketData($ticketData);
 
-                if ($request) {
-                    $outputTicketData = Json::decode($request, 1);
+                    if ($request) {
+                        $outputTicketData = Json::decode($request, 1);
 
-                    $invoice->update([
-                        'send_nimbly' => 1,
-                        'send_nimbly_date' => Carbon::now()->toDateTimeString(),
-                        'id_nimbly_invoice' => $dataInvoice['ID'],
-                        'payload' => Json::encode($params),
-                        'ticket_id' => $outputTicketData['ID'],
-                    ]);
+                        $invoice->update([
+                            'send_nimbly' => 1,
+                            'send_nimbly_date' => Carbon::now()->toDateTimeString(),
+                            'id_nimbly_invoice' => $dataInvoice['ID'],
+                            'payload' => Json::encode($params),
+                            'ticket_id' => $outputTicketData['ID'],
+                        ]);
+                    }
                 }
             }
         }
